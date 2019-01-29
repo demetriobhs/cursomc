2b3304542ff189b3debb2a5677dd357b288b4402
Bruno Demetrio <bdemetriosantos@gmail.com>
2019-01-29 10:42:34 -0200

    Criação e teste da classe de domínio Categoria.

--- src/main/java/br/com/brunodemetrio/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/brunodemetrio/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package br.com.brunodemetrio.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.brunodemetrio.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "REST ok!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		
+		return categorias;
 	}
 
 }
